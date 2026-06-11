47a302ec6715151cfddc21109ffb83632e2f5f49
arieltecay <arieltecay@gmail.com>
2026-06-11 16:51:13 -0300

    Delete unnecesary comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { router } from './routes';
-//add a comment to see if cloudflare is connected
+
 function App() {
   return (
     <>
